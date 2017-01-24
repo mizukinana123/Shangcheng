94e6629566114d4ef7e24536bef06f7eb0828760
yangfan <401135017@qq.com>
2017-01-24 11:51:07 +0800

    rem更换

--- public/libs/rem.js
+++ public/libs/rem.js
@@ -1,23 +1,18 @@
-var oHtml = document.documentElement;
-
-//一开始调用一次
-getSize();
-//当缩放的时候调用方法,实时计算
-window.addEventListener('resize', function() {
-    console.log(oHtml.style.fontsize);
-    getSize();
-});
-
-//获取屏幕的大小
-function getSize() {
-    var screenWidth = oHtml.offsetWidth;
-    if (screenWidth >= 640) {
-        oHtml.style.fontSize = '80px';
-    } else if (screenWidth <= 320) {
-        oHtml.style.fontSize = '40px';
-    } else {
-
-        oHtml.style.fontSize = (screenWidth / 8) + 'px';
+//获取根元素
+var phtml=document.documentElement;
+function getSize(){
+    //实时获取屏幕的大小
+    var rel_phtml=phtml.offsetWidth;
+    if(rel_phtml>640){
+        //这些值是放到手机端哪里试出来的
+        phtml.style.fontSize='47.4074px';
+    }else if(rel_phtml<320){
+        phtml.style.fontSize='23.7037px';
+    }else{
+        //套公式  动态改变根目录文字的大小   每份的大小是100
+        phtml.style.fontSize=rel_phtml/(640/100)+'px';
     }
 
 }
+getSize();
+window.addEventListener('resize',getSize)
